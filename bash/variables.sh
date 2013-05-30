520071a20cec7a91654c0e9a2c11078821acd80c
mx <mx@elektret.de>
2013-05-30 23:10:15 +0200

    Local npm bin path

--- bash/variables.sh
+++ bash/variables.sh
@@ -5,4 +5,5 @@ export TERM=rxvt-unicode
 export EDITOR=vim
 export RUBY_VERSION=`ruby --version | sed 's/ruby \([0-9]*\.[0-9]*\).*/\1/'`
 export GEM_HOME=$HOME/gems/$RUBY_VERSION
-export PATH=$GEM_HOME/bin:$HOME/.local/bin:$HOME/bin:$PATH
+export NPM_HOME=$HOME/npm
+export PATH=$GEM_HOME/bin:$NPM_HOME/bin:$HOME/.local/bin:$HOME/bin:$PATH
