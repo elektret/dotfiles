b1092eb1d306c34460d024f62d96c5fd96c220d1
Manuel <manuel@elektret.de>
2013-09-11 14:25:25 +0200

    Fonts

--- bash/variables.sh
+++ bash/variables.sh
@@ -3,6 +3,7 @@ export LC_CTYPE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export TERM=rxvt-unicode
 export EDITOR=vim
+export HISTSIZE=2000
 export RUBY_VERSION=`ruby --version | sed 's/ruby \([0-9]*\.[0-9]*\).*/\1/'`
 export GEM_HOME=$HOME/gems/$RUBY_VERSION
 export NPM_HOME=$HOME/npm
