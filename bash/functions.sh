061e14f215d91ebdfe40254ccd16cd1691be44c0
Manuel <manuel@elektret.de>
2013-11-20 21:38:15 +0100

    Bash functions

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,11 +1,11 @@
 # Find out the top 10 memory consuming process
 function top10_mem {
-  ps -auxf | sort -nr -k 4 | head -10
+  ps aux | sort -nr -k 4 | head -10 | cut -c -100
 }
 
 # Find out top 10 cpu consuming process
 function top10_cpu {
-  ps -auxf | sort -nr -k 3 | head -10
+  ps aux | sort -nr -k 3 | head -10 | cut -c -100
 }
 
 # Make files readable and directories executable
@@ -15,6 +15,13 @@ function chmod_fs {
   chown -R `id -un`:`id -gn` .
 }
 
+# Prepare files for archiving
+function chmod_archive {
+  chmod_fs
+  find . -type f -exec chmod a-w '{}' \;
+}
+
+# Copy dotfiles
 function copy_dotfiles {
   for object in `find ~/repos/dotfiles -maxdepth 1 -name \[a-z]*`; do
     [ -r ~/.`basename $object` ] && rm -rf ~/.`basename $object`
@@ -23,28 +30,24 @@ function copy_dotfiles {
   done
 }
 
-function copy_profile {
-  for object in `find ~/profile -maxdepth 1 -name \[a-z]*`; do
-    [ -r ~/.`basename $object` ] && rm -rf ~/.`basename $object`
-    cp -rva $object ~/.`basename $object` && [ -f $object ] && chmod a-x ~/.`basename $object`
-    chmod go-rwx ~/.`basename $object`
-  done
-}
-
 # Build mount structure
 function mkdir_dev {
-  for i in a b c d e f; do
-    for j in 1 2 3 4; do
-      mkdir sd${i}${j}
+  for i in a b c d e f
+  do
+    for j in 1 2 3 4
+    do
+      mkdir sd$i$j
     done
   done
 }
 
 # Find expression in Ruby files
 function rgrep {
-  find -L . -type f -name \*.rb -exec grep -n -i -H --color "$1" {} \;
+  find -L . -type f -name '*.rb' -exec grep -n -i -H --color "${1}" {} \;
 }
 
 function qrand {
-  echo `cat /dev/urandom | base91 | head -1`
+  which base91 &> /dev/null
+  [ $? -eq 0 ] && base="base91" || base="base64"
+  echo `cat /dev/urandom | $base | head -1`
 }
