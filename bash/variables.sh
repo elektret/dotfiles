e65b32831babfe4728c7ad99f0be836d614bdd1c
Manuel <manuel@elektret.de>
2015-02-28 23:31:20 +0100

    ENV: PATH

--- bash/variables.sh
+++ bash/variables.sh
@@ -5,5 +5,10 @@ export TERM=xterm-256color
 export PS1='[\u@\h \W]\$ '
 export EDITOR=vim
 export HISTSIZE=2000
-export NPM_HOME=$HOME/npm
-export PATH=$HOME/node_modules/.bin:$HOME/bin:$PATH
+export PERL_MB_OPT="--install_base \"${HOME}/perl5\""
+export PERL_MM_OPT="INSTALL_BASE=${HOME}/perl5"
+export PATH=$HOME/node_modules/.bin:$PATH
+export PATH=$HOME/perl5/bin:$PATH
+export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/.cabal/bin:$PATH
+export PATH=$HOME/bin:$PATH
